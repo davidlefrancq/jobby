7cde29ba452211934ab5ae560b41994df93d0ce6
David Lefrancq <david.lefrancq@gmail.com>
2025-05-23 14:23:46 +0200

    feat: job queue menu position

--- web-manager/src/app/components/JobQueuePanel.tsx
+++ web-manager/src/app/components/JobQueuePanel.tsx
@@ -9,15 +9,12 @@ import JobQueueUnrated from "./JobQueueUnrated";
 export default function JobQueuePanel() {
   return (
     <div className={`grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1`}>
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        <h1 className="text-2xl font-bold">Job Queue</h1>
+      <div className="absolute top-8 left-1/2 -translate-x-1/2 w-1/2 z-50">
+        <JobQueueMenu />
       </div>
-
-      <JobQueueMenu />
 
       <JobQueueUnrated />
       <JobQueueLiked />
-
     </div>
   );
 }
