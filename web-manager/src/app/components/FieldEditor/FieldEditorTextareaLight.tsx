eeecb154a1d2e430fd1161c753e31668238d07b4
David Lefrancq <david.lefrancq@gmail.com>
2025-06-26 09:30:26 +0200

    chore: clean code

--- web-manager/src/app/components/FieldEditor/FieldEditorTextareaLight.tsx
+++ web-manager/src/app/components/FieldEditor/FieldEditorTextareaLight.tsx
@@ -42,7 +42,6 @@ export default function FieldEditorTextareaLight({ className, initialValue, lege
           cols={50}
           value={inputValue || ''}
           onChange={(e) => handleChange(e)}
-          // className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           className="flex-1 w-full px-2 py-1 mr-1 border rounded text-sm focus:outline-none"
           placeholder={legendValue ? legendValue : "Description"}
         />
