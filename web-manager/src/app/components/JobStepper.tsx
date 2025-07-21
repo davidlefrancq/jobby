612912649eb1a118fb6ebb3b500ad1c07df7e402
David Lefrancq <david.lefrancq@gmail.com>
2025-07-21 14:29:30 +0200

    feat: job stepper

---
+++ web-manager/src/app/components/JobStepper.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { Stepper } from "./Stepper";
+import { IStep, StepStatus } from "@/types/IStep";
+
+export default function JobStepper() {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [currentStatus, setCurrentStatus] = useState<StepStatus>("default");
+  const [steps, setSteps] = useState<IStep[]>([
+    { label: "Mails", status: "active" },
+    { label: "Jobs", status: "default" },
+    { label: "Like/Dislike", status: "default" },
+    { label: "Enterprise", status: "default" },
+    { label: "CV", status: "default" },
+    { label: "Letter", status: "default" },
+    { label: "Email", status: "default" },
+    { label: "Gmail Draft", status: "default" },
+  ]);
+
+  useEffect(() => {
+    let status: StepStatus = "default";
+    if (currentStep <= steps.length - 1) status = steps[currentStep].status;
+    if (status !== currentStatus) setCurrentStatus(status);
+  }, [steps]);
+
+
+  const handleStepChange = (index: number, status: "default" | "active" | "success" | "error" | "processing") => {
+    setSteps((prevSteps) =>
+      prevSteps.map((step, i) => (i === index ? { ...step, status } : step))
+    );
+  };
+
+  const simulateStepProgress = () => {
+    if (currentStatus === "active") {
+      handleStepChange(currentStep, "processing");
+      setTimeout(() => {
+        handleStepChange(currentStep, "success");
+        handleStepChange(currentStep + 1, "active");
+        setCurrentStep((prev) => prev + 1);
+      }, 1000); // Simulate a delay for processing 
+    }
+  };
+
+  return (
+    <div className="p-4">
+      <div className="w-full bg-amber-50 p-4 rounded-lg shadow dark:bg-neutral-900">
+        <Stepper steps={steps} />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <button
+          className={`ml-2 px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 ${currentStep === 0 || currentStatus === "processing" ? "opacity-50 cursor-not-allowed" : ""}`}
+          onClick={() => {
+            if (currentStep > 0) {
+              handleStepChange(currentStep, "default");
+              setCurrentStep(currentStep - 1);
+              handleStepChange(currentStep - 1, "active");
+            } else {
+              handleStepChange(currentStep, "default");
+            }
+          }}
+          disabled={currentStep === 0 || currentStatus === "processing"}
+        >
+          Rollback
+        </button>
+        <button
+          className={`px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 ${currentStatus === "processing" || (currentStep >= steps.length - 1 && currentStatus !== "active") ? "opacity-50 cursor-not-allowed" : ""}`}
+          onClick={simulateStepProgress}
+          // disabled={currentStatus === "processing" || currentStep >= steps.length - 1}
+          disabled={currentStatus === "processing" || (currentStep === steps.length - 1 && currentStatus !== "active") || currentStep > steps.length - 1}
+        >
+          Start
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ web-manager/src/app/components/Stepper.tsx
@@ -0,0 +1,74 @@
+import { IStep, StepStatus } from "@/types/IStep";
+import React from "react";
+
+interface StepperProps {
+  steps: IStep[];
+}
+
+export const Stepper = ({ steps }: StepperProps) => {
+  const getStepIcon = (status: StepStatus, index: number) => {
+    switch (status) {
+      case "success":
+        return (
+          <svg className="shrink-0 size-3" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="3">
+            <polyline points="20 6 9 17 4 12" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        );
+      case "error":
+        return (
+          <svg className="shrink-0 size-3" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="3">
+            <path d="M18 6 6 18 M6 6l12 12" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        );
+      case "processing":
+        return (
+          <span
+            className="animate-spin size-4 border-2 border-current border-t-transparent rounded-full"
+            role="status"
+            aria-label="Loading"
+          />
+        );
+      default:
+        return <span>{index + 1}</span>;
+    }
+  };
+
+  return (
+    <div className="w-full">
+      {/* Stepper Header */}
+      <ul className="flex flex-wrap justify-between gap-y-4">
+        {steps.map((step, index) => (
+          <li
+            key={index}
+            className={`flex items-center gap-x-2 shrink basis-0 flex-1 group ${step.status}`}
+          >
+            <span className="min-w-7 min-h-7 inline-flex items-center text-xs align-middle">
+              <span
+                className={`
+                  size-7 flex justify-center items-center shrink-0 rounded-full font-medium
+                  ${
+                    step.status === "active" || step.status === "processing"
+                      ? "bg-blue-600 text-white"
+                      : step.status === "success"
+                      ? "bg-teal-500 text-white"
+                      : step.status === "error"
+                      ? "bg-red-500 text-white"
+                      : "bg-gray-100 text-gray-800 dark:bg-neutral-700 dark:text-white"
+                  }
+                `}
+              >
+                {getStepIcon(step.status, index)}
+              </span>
+              <span className="ms-2 text-sm font-medium text-gray-800 dark:text-white">
+                {step.label}
+              </span>
+            </span>
+            {index < steps.length - 1 && (
+              <div className="w-full h-px flex-1 bg-gray-200 dark:bg-neutral-700"></div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
