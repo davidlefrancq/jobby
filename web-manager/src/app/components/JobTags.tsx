215445b78a270f1a03c35aea017c967f4781e9b7
David Lefrancq <david.lefrancq@gmail.com>
2025-07-29 14:46:38 +0200

    refactor: job tags color

--- web-manager/src/app/components/JobTags.tsx
+++ web-manager/src/app/components/JobTags.tsx
@@ -13,8 +13,8 @@ export default function JobTags({ job }: JobTagsProps) {
     return technologies.includes(tag);
   };
   
-  const cssTechnology = "bg-violet-100 text-violet-900 dark:bg-violet-900 dark:text-gray-100";
-  const cssMethodology = "bg-cyan-100 text-cyan-900 dark:bg-cyan-900 dark:text-gray-100";
+  const cssTechnology = "bg-sky-100 text-sky-900 dark:bg-sky-900 dark:text-gray-100";
+  const cssMethodology = "bg-emerald-100 text-emerald-900 dark:bg-emerald-900 dark:text-gray-100";
 
   return (
     <div className="flex flex-wrap gap-2 mt-2 mb-2">
